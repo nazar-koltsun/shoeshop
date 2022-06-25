8b758640cd7fd37b90ae4cfcafeae0f72cdda8e2
Nazar Koltsun <nazar-koltsun@ukr.net>
2022-06-25 13:14:37 +0300

    Removed additional files

--- server/server.js
+++ server/server.js
@@ -1,12 +1,7 @@
 import express from 'express';
 import products from './data/Products.js';
-import cors from 'cors';
 
 const app = express();
-
-app.use(cors({
-  origin: 'http://localhost:3000'
-}));
 
 // Load products from server
 app.get('/api/products', (reg, res) => {
