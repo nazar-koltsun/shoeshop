ac22fc2450a248ce4d310dda9e23c09cc4d75f72
Nazar Koltsun <nazar-koltsun@ukr.net>
2022-07-07 09:56:50 +0300

    Created routes, inserted data to DataBase

---
+++ server/Routes/ProductRoutes.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import asyncHandler from 'express-async-handler';
+import Product from '../Models/ProductModel.js';
+
+const productRoute = express.Router();
+
+// Get all products
+productRoute.get('/', asyncHandler(async(req, res) => {
+  const products = await Product.find({});
+  res.json(products);
+}))
+
+// Get single product
+productRoute.get('/:id', asyncHandler(async(req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404);
+    throw new Error('Product not found')
+  }
+}))
+
+export default productRoute;
--- server/server.js
+++ server/server.js
@@ -2,22 +2,17 @@ import express from 'express';
 import products from './data/Products.js';
 import dotenv from 'dotenv';
 import connectDataBase from './config/MongoDb.js';
+import ImportData from './Dataimport.js';
+import productRoute from './Routes/ProductRoutes.js'
 
 dotenv.config();
 connectDataBase();
 
 const app = express();
 
-// Load products from server
-app.get('/api/products', (reg, res) => {
-  res.json(products);
-})
-
-// Single product from server
-app.get('/api/products/:id', (reg, res) => {
-  const product = products.find((p) => p._id == reg.params.id);
-  res.json(product);
-})
+// API
+app.use('/api/import', ImportData);
+app.use('/api/products', productRoute);
 
 app.get('/', (reg, res) => {
   res.send('Api is running...');
